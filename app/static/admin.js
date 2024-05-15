10b333cfcc2cbca85c617629a00405dd15f51841
Anna Silinskaya <annsilin@mail.ru>
2024-05-15 17:58:47 +0400

    feat: improve admin page styling

--- app/static/admin.js
+++ app/static/admin.js
@@ -1,7 +1,9 @@
 const dropArea = document.getElementById('drop-area');
 const fileInput = document.getElementById('file-input');
 const uploadBtn = document.getElementById('upload-btn');
-const message = document.getElementById('message');
+const logs = document.getElementById('log');
+const getMoodsCheckbox = document.getElementById('get-moods');
+const getGenresCheckbox = document.getElementById('get-genres');
 
 const handleDragOver = (e) => {
     e.preventDefault();
@@ -18,7 +20,7 @@ const handleDrop = (e) => {
     dropArea.classList.remove('hover');
     const file = e.dataTransfer.files[0];
     if (file.type !== 'text/csv') {
-        message.textContent = 'Invalid file type. Please upload a CSV file.';
+        logs.textContent = 'Invalid file type. Please upload a CSV file.';
         return;
     }
     fileInput.files = file;
@@ -28,7 +30,7 @@ const handleDrop = (e) => {
 const handleInputChange = (e) => {
     const file = e.target.files[0];
     if (file.type !== 'text/csv') {
-        message.textContent = 'Invalid file type. Please upload a CSV file.';
+        logs.textContent = 'Invalid file type. Please upload a CSV file.';
         return;
     }
     uploadBtn.disabled = false;
@@ -45,16 +47,13 @@ const uploadFile = async () => {
     const formData = new FormData();
     formData.append('file', file);
 
-    const getMoodsCheckbox = document.getElementById('get-moods');
-    const getGenresCheckbox = document.getElementById('get-genres');
-
     formData.append('get-moods', getMoodsCheckbox.checked);
     formData.append('get-genres', getGenresCheckbox.checked);
 
     uploadBtn.disabled = true;
     getMoodsCheckbox.disabled = true;
     getGenresCheckbox.disabled = true;
-    message.textContent = 'Uploading...';
+    logs.textContent = 'Uploading...';
 
     try {
         const response = await fetch('/process-csv', {
@@ -69,7 +68,7 @@ const uploadFile = async () => {
         const data = await response.json();
 
         if (response.ok) {
-            message.textContent = data.message;
+            logs.textContent = data.message;
             fileInput.value = '';
             uploadBtn.disabled = true;
             getMoodsCheckbox.disabled = false;
@@ -79,7 +78,7 @@ const uploadFile = async () => {
         }
     } catch (error) {
         console.error('Error uploading file:', error.message);
-        message.textContent = `Error uploading file: ${error.message}`;
+        logs.textContent = `Error uploading file: ${error.message}`;
         uploadBtn.disabled = false;
         getMoodsCheckbox.disabled = false;
         getGenresCheckbox.disabled = false;
@@ -92,4 +91,19 @@ dropArea.addEventListener('dragleave', handleDragLeave);
 dropArea.addEventListener('drop', handleDrop);
 fileInput.addEventListener('change', handleInputChange);
 uploadBtn.addEventListener('click', uploadFile);
-
+getMoodsCheckbox.addEventListener('change', (e) => {
+    if (e.target.checked) {
+        document.querySelector('.table-block_features').removeAttribute('hidden');
+        document.querySelector('.table-block_predictions').setAttribute('hidden', 'true')
+    } else {
+        document.querySelector('.table-block_features').setAttribute('hidden', 'true')
+        document.querySelector('.table-block_predictions').removeAttribute('hidden');
+    }
+})
+getGenresCheckbox.addEventListener('change', (e) => {
+    if (e.target.checked) {
+        document.querySelector('.table-block_genre').setAttribute('hidden', 'true');
+    } else {
+        document.querySelector('.table-block_genre').removeAttribute('hidden');
+    }
+})
